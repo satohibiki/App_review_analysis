b7cc939d4b641c70ae469951a0edb50d389ec0e1
satohibiki <sato.hibiki@icloud.com>
2023-11-07 18:19:32 +0900

    グラフの作成

--- visualization/review_app/views.py
+++ visualization/review_app/views.py
@@ -2,6 +2,11 @@ from review_app import app
 from flask import render_template, request
 import csv
 import os
+from datetime import datetime as dt
+from datetime import timedelta
+from matplotlib.figure import Figure
+import base64
+from io import BytesIO
 
 app_names = ['capcut', 
          'coke_on', 
@@ -17,6 +22,26 @@ app_names = ['capcut',
          '楽天ペイ',
          'buzzvideo']
 
+def date_range(start, stop, step = timedelta(1)):
+    current = dt.strptime(start, '%Y-%m-%d %H:%M:%S')
+    current = current.date()
+    stop = dt.strptime(stop, '%Y-%m-%d %H:%M:%S')
+    stop = stop.date()
+    print(current)
+    while current < stop:
+        yield current
+        current += step
+
+def twitter_date_range(start, stop, step = timedelta(1)):
+    current = dt.strptime(start, "%Y-%m-%dT%H:%M:%S.%fZ")
+    current = current.date()
+    stop = dt.strptime(stop, "%Y-%m-%dT%H:%M:%S.%fZ")
+    stop = stop.date()
+    print(current)
+    while current < stop:
+        yield current
+        current += step
+
 @app.route('/')
 def index():
     return render_template("index.html", app_names=app_names)
@@ -28,6 +53,7 @@ def google_read(app_name):
     app = app_name
     rows = []
     clusters = []
+    date_graph_list = []
 
     path = f'../クラスタリング/google_{app}.csv'
     is_file = os.path.isfile(path)
@@ -36,10 +62,12 @@ def google_read(app_name):
             with open(path, 'r', encoding='utf-8-sig') as csv_file:
                 csv_reader = csv.reader(csv_file)
                 rows = list(csv_reader)
+                rows = sorted(rows, reverse=False, key=lambda x: x[2]) # 日付で並び替え
         else:
             with open(path, 'r', encoding='utf-8-sig') as csv_file:
                 csv_reader = csv.reader(csv_file)
                 rows_list = list(csv_reader)
+                rows_list = sorted(rows_list, reverse=False, key=lambda x: x[2]) # 日付で並び替え
                 # 日付範囲内のアイテムをフィルタリング
                 result = []
                 for row in rows_list:
@@ -52,7 +80,28 @@ def google_read(app_name):
         clusters.append([index, count, title])
     clusters = sorted(clusters, reverse=True, key=lambda x: x[1])
 
-    return render_template("detail_google.html", app_names=app_names, rows=rows, app=app, clusters=clusters)
+    if rows != []:
+        for date in date_range(rows[0][2], rows[-1][2]):
+            count = sum(1 for row in rows if dt.strptime(row[2], '%Y-%m-%d %H:%M:%S').date() == date)
+            date_graph_list.append([date, count])
+        dates = [item[0] for item in date_graph_list]
+        counts = [item[1] for item in date_graph_list]
+
+        #グラフ描画
+        fig = Figure(figsize=(10, 3))
+        ax = fig.subplots()
+        ax.plot(dates, counts, marker='o', linestyle='-', markersize=5)
+
+        #画像をバッファに保存
+        buf = BytesIO()
+        fig.savefig(buf, format="png")
+
+        #画像データをBase64にエンコード
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    else:
+        data = ""
+
+    return render_template("detail_google.html", app_names=app_names, rows=rows, app=app, clusters=clusters, data=data)
 
 @app.route('/twitter/detail/<string:app_name>')
 def twitter_read(app_name):
@@ -61,6 +110,7 @@ def twitter_read(app_name):
     app = app_name
     rows = []
     clusters = []
+    date_graph_list = []
 
     path = f'../クラスタリング/twitter_{app}.csv'
     is_file = os.path.isfile(path)
@@ -69,10 +119,12 @@ def twitter_read(app_name):
             with open(path, 'r', encoding='utf-8-sig') as csv_file:
                 csv_reader = csv.reader(csv_file)
                 rows = list(csv_reader)
+                rows = sorted(rows, reverse=False, key=lambda x: x[2]) # 日付で並び替え
         else:
             with open(path, 'r', encoding='utf-8-sig') as csv_file:
                 csv_reader = csv.reader(csv_file)
                 rows_list = list(csv_reader)
+                rows_list = sorted(rows_list, reverse=False, key=lambda x: x[2]) # 日付で並び替え
                 # 日付範囲内のアイテムをフィルタリング
                 result = []
                 for row in rows_list:
@@ -85,4 +137,25 @@ def twitter_read(app_name):
         clusters.append([index, count, title])
     clusters = sorted(clusters, reverse=True, key=lambda x: x[1])
 
-    return render_template("detail_twitter.html", app_names=app_names, rows=rows, app=app, clusters=clusters)
+    if rows != []:
+        for date in twitter_date_range(rows[0][2], rows[-1][2]):
+            count = sum(1 for row in rows if dt.strptime(row[2], "%Y-%m-%dT%H:%M:%S.%fZ").date() == date)
+            date_graph_list.append([date, count])
+        dates = [item[0] for item in date_graph_list]
+        counts = [item[1] for item in date_graph_list]
+
+        #グラフ描画
+        fig = Figure(figsize=(10, 3))
+        ax = fig.subplots()
+        ax.plot(dates, counts, marker='o', linestyle='-', markersize=5)
+
+        #画像をバッファに保存
+        buf = BytesIO()
+        fig.savefig(buf, format="png")
+
+        #画像データをBase64にエンコード
+        data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    else:
+        data = ""
+
+    return render_template("detail_twitter.html", app_names=app_names, rows=rows, app=app, clusters=clusters, data=data)
