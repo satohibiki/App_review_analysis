ca7f86f60d36aa72b6eb5adfb3a5aa89d023020d
satohibiki <sato.hibiki@icloud.com>
2023-11-10 00:53:00 +0900

    リファクタリング

--- visualization/review_app/views.py
+++ visualization/review_app/views.py
@@ -32,11 +32,13 @@ def date_range(start, stop, step = timedelta(1)):
 def index():
     start_date = request.args.get('start-date', '2021-10-21')
     end_date = request.args.get('end-date', '2021-12-15')
+    google_rows = []
+    twitter_rows = []
     google_graphs = []
     twitter_graphs = []
 
     for app_name in app_names:
-        # google用のグラフ作成
+        # GooglePlayストアのレビューのリストを作成
         path = f'../クラスタリング/google_{app_name}.csv'
         is_file = os.path.isfile(path)
         if is_file:
@@ -44,20 +46,24 @@ def index():
                 google_csv_reader = csv.reader(google_csv_file)
                 google_rows = list(google_csv_reader)
                 google_rows = sorted(google_rows, reverse=False, key=lambda x: x[2]) # 日付で並び替え
-                if start_date is not None or end_date is not None: # 日付範囲内のアイテムをフィルタリング
-                    result = []
-                    for row in google_rows:
-                        if start_date <= row[2][:10] <= end_date:
-                            result.append(row)
-                    google_rows = result
-        if google_rows != []:
-            google_date_list = []
-            for date in date_range(start_date, end_date):
-                count = sum(1 for row in google_rows if dt.strptime(row[2], '%Y-%m-%d %H:%M:%S').date() == date)
-                google_date_list.append([date.strftime('%Y/%m/%d'), count])
-            google_graphs.append(google_date_list)
 
-        # twitter用のグラフ作成
+        # 検索結果のリスト作成
+        search_result = []
+        for row in google_rows:
+            if start_date <= row[2][:10] <= end_date:
+                search_result.append(row)
+        google_rows = search_result
+
+        # 日付ごとのレビュー数のリストを作成
+        google_date_list = []
+        for date in date_range(start_date, end_date):
+            count = sum(1 for row in google_rows if dt.strptime(row[2], '%Y-%m-%d %H:%M:%S').date() == date)
+            google_date_list.append([date.strftime('%Y/%m/%d'), count])
+        google_graphs.append(google_date_list)
+
+
+
+        # ツイートのリスト作成
         path = f'../クラスタリング/twitter_{app_name}.csv'
         is_file = os.path.isfile(path)
         if is_file:
@@ -65,18 +71,20 @@ def index():
                 twitter_csv_reader = csv.reader(twitter_csv_file)
                 twitter_rows = list(twitter_csv_reader)
                 twitter_rows = sorted(twitter_rows, reverse=False, key=lambda x: x[2]) # 日付で並び替え
-                if start_date is not None or end_date is not None: # 日付範囲内のアイテムをフィルタリング
-                    result = []
-                    for row in twitter_rows:
-                        if start_date <= row[2][:10] <= end_date:
-                            result.append(row)
-                    twitter_rows = result
-        if twitter_rows != []:
-            twitter_date_list = []
-            for date in date_range(start_date, end_date):
-                count = sum(1 for row in twitter_rows if dt.strptime(row[2], "%Y-%m-%dT%H:%M:%S.%fZ").date() == date)
-                twitter_date_list.append([date.strftime('%Y/%m/%d'), count])
-            twitter_graphs.append(twitter_date_list)
+
+        # 検索結果のリスト作成
+        search_result = []
+        for row in twitter_rows:
+            if start_date <= row[2][:10] <= end_date:
+                search_result.append(row)
+        twitter_rows = search_result
+
+        # 日付ごとのレビュー数のリストを作成
+        twitter_date_list = []
+        for date in date_range(start_date, end_date):
+            count = sum(1 for row in twitter_rows if dt.strptime(row[2], "%Y-%m-%dT%H:%M:%S.%fZ").date() == date)
+            twitter_date_list.append([date.strftime('%Y/%m/%d'), count])
+        twitter_graphs.append(twitter_date_list)
 
     return render_template("index.html", 
                            app_names=app_names,
@@ -93,8 +101,9 @@ def read(category, app_name):
     category = category
     rows = []
     clusters = []
-    date_graph_list = []
+    graphs = []
 
+    # 対象カテゴリーのレビューのリストを作成
     path = f'../クラスタリング/{category}_{app}.csv'
     is_file = os.path.isfile(path)
     if is_file:
@@ -102,38 +111,36 @@ def read(category, app_name):
             csv_reader = csv.reader(csv_file)
             rows = list(csv_reader)
             rows = sorted(rows, reverse=False, key=lambda x: x[2]) # 日付で並び替え
-            if start_date is not None or end_date is not None: # 日付範囲内のアイテムをフィルタリング
-                result = []
-                for row in rows:
-                    if start_date <= row[2][:10] <= end_date:
-                        result.append(row)
-                rows = result
+    
+    # 検索結果のリスト作成
+    search_result = []
+    for row in rows:
+        if start_date <= row[2][:10] <= end_date:
+            search_result.append(row)
+    rows = search_result
 
     # クラスタリング
     count_dict = {}
     for item in rows:
-        key = item[5]  # 6番目の要素をキーとします
+        key = item[5]
         if key in count_dict:
             count_dict[key] += 1
         else:
             count_dict[key] = 1
-    
     clusters = [[key, value] for key, value in count_dict.items()]
     for cluster in clusters:
         title = next(row[4] for row in rows if row[5] == cluster[0])
         cluster.append(title)
     clusters = sorted(clusters, reverse=True, key=lambda x: x[1])
-
     top_review = clusters[:10]
 
-    # 日時リスト作成
-    if rows != []:
-        for date in date_range(start_date, end_date):
-            if category=="google":
-                count = sum(1 for row in rows if dt.strptime(row[2], '%Y-%m-%d %H:%M:%S').date() == date)
-            else:
-                count = sum(1 for row in rows if dt.strptime(row[2], "%Y-%m-%dT%H:%M:%S.%fZ").date() == date)
-            date_graph_list.append([date.strftime('%Y/%m/%d'), count])
+    # 日付ごとのレビュー数のリストを作成
+    for date in date_range(start_date, end_date):
+        if category=="google":
+            count = sum(1 for row in rows if dt.strptime(row[2], '%Y-%m-%d %H:%M:%S').date() == date)
+        else:
+            count = sum(1 for row in rows if dt.strptime(row[2], "%Y-%m-%dT%H:%M:%S.%fZ").date() == date)
+        graphs.append([date.strftime('%Y/%m/%d'), count])
 
     return render_template("detail.html", 
                            app_names=app_names, 
@@ -141,7 +148,7 @@ def read(category, app_name):
                            app=app, 
                            category=category, 
                            clusters=clusters, 
-                           date_graph_list=date_graph_list, 
+                           graphs=graphs, 
                            top_review=top_review, 
                            start_date=start_date, 
                            end_date=end_date)
