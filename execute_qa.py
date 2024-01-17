9825ec5d4a0346b24dffa917e990eda82c816db2
satohibiki <sato.hibiki@icloud.com>
2024-01-17 15:45:43 +0900

    卒論の修正、追加

--- execute_qa.py
+++ execute_qa.py
@@ -7,8 +7,8 @@ from tqdm import tqdm
 model = AutoModelForQuestionAnswering.from_pretrained('output/')  
 tokenizer = BertJapaneseTokenizer.from_pretrained('cl-tohoku/bert-base-japanese-whole-word-masking') 
 
-def exerute_answer(context, question):
-    # 推論の実行
+def execute_answer(context, question):
+    # 自動抽出の実行
     inputs = tokenizer.encode_plus(question, context, add_special_tokens=True, return_tensors="pt")
     input_ids = inputs["input_ids"].tolist()[0]
     output = model(**inputs)
@@ -35,7 +35,7 @@ def create_answer_twitter(app_name):
                 context = row[3]
                 question = f"この文はTwitterのツイートです。{app_name}アプリの欠陥や{app_name}アプリに対する要望が書かれているのはどこですか？"
                 # 予測結果をモデルから取得
-                prediction = exerute_answer(context, question)
+                prediction = execute_answer(context, question)
                 if prediction != '[CLS]':
                     if "[CLS]" in prediction:
                         prediction = prediction.replace(f"[CLS]この文はTwitterのツイートです。{app_name}アプリの欠陥や{app_name}アプリに対する要望が書かれているのはどこですか?[SEP]", '')
@@ -55,7 +55,7 @@ def create_answer_google(app_name):
                 context = row[3]
                 question = f"この文章はGooglePlayストアのレビューです。{app_name}アプリの欠陥や{app_name}アプリに対する要望が書かれているのはどこですか？"
                 # 予測結果をモデルから取得
-                prediction = exerute_answer(context, question)
+                prediction = execute_answer(context, question)
                 if prediction != '[CLS]':
                     if "[CLS]" in prediction:
                         prediction = prediction.replace(f"[CLS]この文章はGooglePlayストアのレビューです。{app_name}アプリの欠陥や{app_name}アプリに対する要望が書かれているのはどこですか?[SEP]", '')
